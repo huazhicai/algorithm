893c0778fd200424f34e48acd9998258852916d4
zhihua cai <zhihua cai>
2018-05-15 08:12:45 +0800

    ch08

---
+++ ch08/maxFact.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+'''
+$maxFact.py -- calculates the largest factor of a number
+    (or indicate that it is prime)
+'''
+
+
+# calcMaxFac(num) -- return -1 if prime, largest factor otherwise
+def calcMaxFac(num):
+    # composite smallest possible factor is 2
+    count = num // 2
+
+    # count backwards to 0 looking for first factor
+    while count > 1:
+
+        # break if factor found...
+        if (num % count == 0):
+            break
+
+        # otherwise decrement and continue
+        else:
+            count -= 1
+
+    # reached 1 without finding a factor, ergo prime;
+    # (example of while-else statement)
+    else:
+        return -1
+
+    return count
+
+
+# showMaxFac(x, y) -- return largest factors of numbers from x to y,
+# or an indication a number if prime if applicable
+def showMaxFacs(x, y):
+    for eachNum in range(x, y):
+        res = calcMaxFac(eachNum)
+        if res == -1:
+            print(eachNum, 'is prime')
+        else:
+            print(eachNum, 'has a largest factor of', res)
+
+
+# show the largest factors of values 10 - 20 as a test
+if __name__ == '__main__':
+    showMaxFacs(10, 21)
